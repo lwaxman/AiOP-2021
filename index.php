a1e2e56be43a031ca05e3efb81265d45c6e646d5
Laurie Waxman <laurie@midnightcommercial.com>
2021-02-23 17:39:20 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<html>
+	<head>
+		<title>AiOP 2021: NORMAL</title>
+		<!-- meta data -->
+		<meta charset="UTF-8">
+		<meta name="description" content="Art in Odd Places 2021: Normal">
+		<meta name="keywords" content="AiOP, Festival, Performance, New York City">
+		<meta name="author" content="Laurie Waxman">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- fonts -->
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;600&family=Roboto:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.typekit.net/yxs1iyp.css">
+		<!-- styles -->
+		<link rel="stylesheet" href="_styles/style.css">
+	</head>
+	<body>
+		<nav></nav>
+		<main class="home">
+			<img src="assets/Grid-Home.svg" alt="grid" class="background-img">		
+			<section class="home header">
+				<h1>NORMAL</h1>
+			</section>
+			<section class="home schedule">
+				<div>
+					<h2>May 14-16</h2>
+					<a class="button primary" href="/schedule">View schedule</a>
+				</div>
+			</section>
+			<section class="home about">
+				<div>
+					<p class="p1">Curated by Furusho von Puttkammer, <span class="italic">AiOP 2021: NORMAL</span> will present artists who seek to critique the mythos of the American Dream and the history of American politics.</p>
+					<a class="button primary" href="/about">More about it</a>
+				</div>
+			</section>
+			<section class="home artists">
+				<div>
+					<h2>Participating artists & groups</h2>
+					<a class="button primary" href="/artists">Explore projects</a>
+					<a class="button secondary" href="/schedule">Views schedule</a>
+				</div>
+			</section>
+		</main>
+		<footer>
+			<div class="footer-col">
+				<p><a href="http://artinoddplaces.org">artinoddplaces.org</a></p>
+				<div class="social">
+					<div class="social-icon">FB</div>
+					<div class="social-icon">IG</div>
+					<div class="social-icon">TW</div>
+				</div>
+			</div>
+			<div class="footer-col">
+				<p>May 14-16 2021</p>
+				<p>New York City</p>
+			</div>
+			<div class="footer-col">
+				<p>14th Street from Avenue C to the Hudson River</p>
+			</div>
+			<div class="footer-col">
+				<p>Furusho von Puttkammer</p>
+				<p class="light">Curator</p>
+			</div>
+			<div class="footer-col">
+				<p>Ed Woodham</p>
+				<p class="light">Founder & Director</p>
+			</div>
+			<div class="footer-col">
+				<p>Laurie Waxman</p>
+				<p class="light">Website</p>
+			</div>
+			<div class="footer-col">
+				<p>© 2021 Art in Odd Places</p>
+				<p>All Rights Reserved</p>
+			</div>
+		</footer>
+	</body>
+</html>
