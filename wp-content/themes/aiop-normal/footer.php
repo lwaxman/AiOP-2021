29efec1ab985922fc61bd81c57dd28a3e906fae6
Laurie Waxman <laurie@midnightcommercial.com>
2021-04-18 11:37:18 -0400

    updated footer & menu icon

--- wp-content/themes/aiop-normal/footer.php
+++ wp-content/themes/aiop-normal/footer.php
@@ -1,38 +1,41 @@
 		<footer>
-			<div class="footer-aiop">	
 
-				<div class="footer-col" id="footer-web">
-					<p><a href="http://artinoddplaces.org">artinoddplaces.org</a></p>
-					<div class="social">
-						<a href="https://www.instagram.com/artinoddplaces" class="social-icon instagram" target="_blank" rel="noopener noreferrer" title="Instagram"><svg id="socialInstagram" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 51.69 51.56"><path d="M15,0A15,15,0,0,0,0,15V36.6a15,15,0,0,0,15,15H36.73a15,15,0,0,0,15-15V15a15,15,0,0,0-15-15Zm0,4.43H36.73A10.46,10.46,0,0,1,47.26,15V36.6A10.46,10.46,0,0,1,36.73,47.13H15A10.46,10.46,0,0,1,4.43,36.6V15A10.46,10.46,0,0,1,15,4.43ZM39.72,8.87A3.1,3.1,0,1,0,42.82,12,3.09,3.09,0,0,0,39.72,8.87ZM25.84,12.51A13.28,13.28,0,1,0,39.12,25.78,13.3,13.3,0,0,0,25.84,12.51Zm0,4.43A8.84,8.84,0,1,1,17,25.78,8.81,8.81,0,0,1,25.84,16.94Z"/></svg></a>
-						<a href="https://www.facebook.com/artinoddplacesnyc" class="social-icon instagram" target="_blank" rel="noopener noreferrer" title="Facebook"><svg id="socialFacebook" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.79 50.79"><path d="M2.8,0A2.8,2.8,0,0,0,0,2.8V48a2.8,2.8,0,0,0,2.8,2.8H27.13V31.13H20.51V23.46h6.62V17.8c0-6.56,4-10.13,9.86-10.13A51.77,51.77,0,0,1,42.9,8v6.85H38.84c-3.18,0-3.8,1.51-3.8,3.73v4.9h7.6l-1,7.67H35V50.79H48a2.8,2.8,0,0,0,2.8-2.8V2.8A2.8,2.8,0,0,0,48,0Z"/></svg></a>
-						<a href="mailto:aiopnyc@gmail.com" class="social-icon email" target="_blank" rel="noopener noreferrer" title="Email"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 43.9 31.84"><path class="cls-1" d="M39.11,0H4.79A4.8,4.8,0,0,0,0,4.79V27.05a4.8,4.8,0,0,0,4.79,4.79H39.11a4.8,4.8,0,0,0,4.79-4.79V4.79A4.8,4.8,0,0,0,39.11,0ZM30.65,15.15l11-10.34V27.05a2.31,2.31,0,0,1-.06.52ZM39.11,2.26a2.44,2.44,0,0,1,1.45.47L22,20.23,3.34,2.73a2.44,2.44,0,0,1,1.45-.47ZM2.31,27.57a3.05,3.05,0,0,1-.05-.52V4.82l11,10.33Zm2.48,2a2.59,2.59,0,0,1-1-.23L14.89,16.7l6.29,5.9a1.11,1.11,0,0,0,1.54,0L29,16.7,40.15,29.35a2.43,2.43,0,0,1-1,.23Z"/></svg></a>
-					</div>
-				</div>
-				<div class="footer-col" id="footer-date">
-					<p>May 14-16 2021</p>
-					<p>New York City</p>
-				</div>
-				<div class="footer-col" id="footer-location">
-					<p>14th Street from Avenue C to the Hudson River</p>
-				</div>
-				<div class="footer-col" id="footer-curator">
-					<p>Furusho von Puttkammer</p>
-					<p class="light">Curator</p>
-				</div>
-				<div class="footer-col" id="footer-founder">
-					<p>Ed Woodham</p>
-					<p class="light">Founder & Director</p>
-				</div>
+			<div class="footer-col" id="footer-logo">
+				<a href="<?php echo get_home_url(); ?>">
+					<img src="<?php bloginfo('template_url'); ?>/assets/AiOP2021_classic_rgb_r.png" alt="" id="aiopLogo" alt="Art in Odd Places">
+				</a>
 			</div>
-
-			<div class="footer-col footer-credit" id="footer-credit">
-				<p>Laurie Waxman</p>
-				<p class="light">Website & Design</p>
+			<div class="footer-col" id="footer-festival">
+				<p>May 14-16 2021</p>
+				<p><span class="light">New York City</span></p>
+				<p><span class="light">14th Street from Avenue C to the Hudson River</span></p>
+			</div>
+			<div class="footer-col" id="footer-people">
+				<p>Furusho von Puttkammer</br>
+					<span class="light">Curator</span>
+				</p>
+				<p>Ed Woodham</br>
+					<span class="light">Founder & Director</span>
+				</p>
+				<p><a href="https://lauriewaxman.com" target="_blank">Laurie Waxman</a></br>
+					<span class="light">Website & Design</span>
+				</p>
+			</div>
+			<div class="footer-col" id="footer-contact">
+				<p>Contact</br>
+					<span class="light"><a href="mailto:aiopnyc@gmail.com" target="_blank">aiopnyc@gmail.com</a></span>
+				</p>
+			</div>          
+			<div class="footer-col" id="footer-social">
+				<div class="social">
+					<a href="https://www.instagram.com/artinoddplaces" class="social-icon instagram" target="_blank" rel="noopener noreferrer" title="Instagram"><svg id="socialInstagram" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 51.73 51.6"><path class="cls-1" d="M25.84,16.94h-.06a8.87,8.87,0,1,0,.06,0Zm0,0h-.06a8.87,8.87,0,1,0,.06,0ZM36.73,0H15A15,15,0,0,0,0,15V36.6a15,15,0,0,0,15,15H36.73a15,15,0,0,0,15-15V15A15,15,0,0,0,36.73,0Zm2.39,25.79A13.28,13.28,0,1,1,25.84,12.51,13.27,13.27,0,0,1,39.12,25.79Zm.6-10.72a3.1,3.1,0,0,1,0-6.2h.05a3.1,3.1,0,0,1-.05,6.2ZM25.84,16.94h-.06a8.87,8.87,0,1,0,.06,0Zm0,0h-.06a8.87,8.87,0,1,0,.06,0Zm0,0h-.06a8.87,8.87,0,1,0,.06,0Z"/></svg></a>
+					<a href="https://www.facebook.com/artinoddplacesnyc" class="social-icon instagram" target="_blank" rel="noopener noreferrer" title="Facebook"><svg id="socialFacebook" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.79 50.79"><path d="M2.8,0A2.8,2.8,0,0,0,0,2.8V48a2.8,2.8,0,0,0,2.8,2.8H27.13V31.13H20.51V23.46h6.62V17.8c0-6.56,4-10.13,9.86-10.13A51.77,51.77,0,0,1,42.9,8v6.85H38.84c-3.18,0-3.8,1.51-3.8,3.73v4.9h7.6l-1,7.67H35V50.79H48a2.8,2.8,0,0,0,2.8-2.8V2.8A2.8,2.8,0,0,0,48,0Z"/></svg></a>
+				</div>
 			</div>
 			<div class="footer-col footer-copyright" id="footer-copyright">
-				<p>© 2021 Art in Odd Places</p>
-				<p>All rights reserved</p>
+				<p>© 2021 Art in Odd Places</br>
+				All rights reserved</p>
+				<p><span class="light">Art in Odd Places is a project of <a href="http://gohproductions.org/">GOH Productions</a>.</span></p>
 			</div>
 
 		</footer>
